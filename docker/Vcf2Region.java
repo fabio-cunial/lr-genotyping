e36bea3e94aa52e877dffd515ae090f53d33136a
Fabio Cunial <fcunial@broadinstitute.org>
2023-03-05 22:12:36 -0500

    minor fix

---
+++ docker/Vcf2Region.java
@@ -0,0 +1,164 @@
+import java.io.*;
+
+
+/**
+ * 
+ */
+public class Vcf2Region {
+    /**
+     * @param args 0: VCF file with just the name of each column and a single
+     * SV call.
+     */
+    public static void main(String[] args) throws IOException {
+        final String VCF_FILE = args[0];
+        final int HORIZONTAL_SLACK = Integer.parseInt(args[1]);
+        final int CHROMOSOME_LENGTH = Integer.parseInt(args[2]);
+        
+        int frameStart, frameEnd, svStart, svLength;
+        String str, chromosome;
+        BufferedReader br;
+        String[] tokens;
+        
+        br = new BufferedReader(new FileReader(VCF_FILE));
+        br.readLine(); str=br.readLine();
+        br.close();
+        tokens=str.split("\t");
+        chromosome=tokens[0];
+        svStart=Integer.parseInt(tokens[1]);
+        frameStart=svStart-HORIZONTAL_SLACK;
+        if (frameStart<0) frameStart=0;
+        frameEnd=-1; svLength=-1;
+        str=getField(tokens[7],SVLEN_STR);
+        if (str!=null) {
+            svLength=Integer.parseInt(str);
+            frameEnd=svStart+svLength+HORIZONTAL_SLACK;
+        }
+        if (frameEnd==-1) {
+            str=getField(tokens[7],END_STR);
+            if (str!=null) frameEnd=Integer.parseInt(str)+HORIZONTAL_SLACK;
+        }
+        if (frameEnd==-1) frameEnd=svStart+HORIZONTAL_SLACK;
+        if (frameEnd>CHROMOSOME_LENGTH) frameEnd=CHROMOSOME_LENGTH;    
+        System.out.println(chromosome+":"+frameStart+"-"+frameEnd);
+    }
+    
+    
+	/**
+	 * Basic constants
+	 */
+	public static final char COMMENT = '#';
+	public static final String SEPARATOR = ";";
+	public static final String PASS_STR = "PASS";
+	public static final String PRECISE_STR = "PRECISE";
+	public static final String IMPRECISE_STR = "IMPRECISE";
+	public static final String END_STR = "END";
+	public static final String SVTYPE_STR = "SVTYPE";
+	public static final String SVLEN_STR = "SVLEN";
+	public static final String CHR2_STR = "CHR2";
+	public static final String CT_STR = "CT";
+	public static final String CT_325_STR = "'3to5'";
+	public static final String CT_523_STR = "'5to3'";
+	public static final String CT_525_STR = "'5to5'";
+	public static final String CT_323_STR = "'3to3'";
+    
+	/**
+	 * SV types: labels used by callers.
+	 */
+	public static final String DEL_STR = "DEL";
+	public static final String DEL_ME_STR = "DEL:ME";
+	public static final String DEL_INV_STR = "DEL/INV";
+	public static final String INS_STR = "INS";
+	public static final String INS_ME_STR = "INS:ME";
+	public static final String INS_NOVEL_STR = "INS:NOVEL";
+	public static final String DUP_STR = "DUP";
+	public static final String DUP_TANDEM_STR = "DUP:TANDEM";
+	public static final String DUP_INT_STR = "DUP:INT";
+	public static final String INV_STR = "INV";
+	public static final String INV_DUP_STR = "INVDUP";
+	public static final String CNV_STR = "CNV";
+	public static final String BND_STR = "BND";
+	public static final String TRA_STR = "TRA";
+    
+	/**
+     *
+	 */
+	public static final byte TYPE_INSERTION = 1;
+	public static final byte TYPE_DELETION = 2;
+	public static final byte TYPE_DEL_INV = 3;
+	public static final byte TYPE_INVERSION = 4;
+	public static final byte TYPE_INV_DUP = 5;
+	public static final byte TYPE_DUPLICATION = 6;
+	public static final byte TYPE_CNV = 7;
+	public static final byte TYPE_BREAKEND = 8;
+	public static final byte TYPE_TRANSLOCATION = 9;
+    
+	/**
+	 * Confidence intervals of positions.
+	 *
+	 * Remark: some callers report a standard deviation instead of a confidence
+	 * interval. Sniffles reports additional interval information in its BEDPE
+	 * output (an alternative to VCF), which our programs disregard for 
+	 * simplicity. Some callers use CILEN to express a "confidence interval 
+	 * around inserted/deleted material between breakends": we interpret CILEN
+	 * exactly like CIEND, and we ignore it for insertions (since representing
+	 * variable-length insertion strings complicates our code).
+	 */
+	public static final String CI_SEPARATOR = ",";
+	public static final String CIPOS_STR = "CIPOS";
+	public static final String CIEND_STR = "CIEND";
+	public static final String STD_START1_STR = "STD_quant_start";
+	public static final String STD_START2_STR = "STD_POS1";
+	public static final String STD_END1_STR = "STD_quant_stop";
+	public static final String STD_END2_STR = "STD_POS2";
+	public static final String CILEN_STR = "CILEN";
+	public static final int CIPOS_STR_LENGTH = CIPOS_STR.length();
+	public static final int CIEND_STR_LENGTH = CIEND_STR.length();
+	public static final int STD_START1_STR_LENGTH = STD_START1_STR.length();
+	public static final int STD_START2_STR_LENGTH = STD_START2_STR.length();
+	public static final int STD_END1_STR_LENGTH = STD_END1_STR.length();
+	public static final int STD_END2_STR_LENGTH = STD_END2_STR.length();
+	public static final int CILEN_STR_LENGTH = CILEN_STR.length();
+    
+    
+	/**
+	 * @return NULL if $field$ does not occur in $str$.
+	 */
+	public static final String getField(String str, String field) {
+		final int FIELD_LENGTH = field.length()+1;
+		int p = str.indexOf(field+"=");
+		if (p<0) return null;
+		if (field.equalsIgnoreCase(END_STR)) {
+			while (p>=2 && str.substring(p-2,p-2+CIEND_STR.length()).equalsIgnoreCase(CIEND_STR)) p=str.indexOf(field+"=",p+1);
+			if (p<0) return null;
+		}
+		final int q = str.indexOf(SEPARATOR,p+FIELD_LENGTH);
+		return str.substring(p+FIELD_LENGTH,q<0?str.length():q);
+	}
+    
+    
+	/**
+	 * @return -1 iff the type cannot be determined.
+	 */
+	private static final int getType_infoField(String type) {
+		if (type==null || type.length()==0) return -1;
+		if ( type.equalsIgnoreCase(DEL_STR) || 
+			 type.equalsIgnoreCase(DEL_ME_STR)
+		   ) return TYPE_DELETION;
+		else if (type.equalsIgnoreCase(DEL_INV_STR)) return TYPE_DEL_INV;
+		else if ( type.equalsIgnoreCase(INS_STR) || 
+			      type.equalsIgnoreCase(INS_ME_STR) || 
+				  type.equalsIgnoreCase(INS_NOVEL_STR)
+				) return TYPE_INSERTION;
+		else if ( type.equalsIgnoreCase(DUP_STR) ||
+			      type.equalsIgnoreCase(DUP_TANDEM_STR) ||
+				  type.equalsIgnoreCase(DUP_INT_STR)
+			    ) return TYPE_DUPLICATION;
+		else if (type.equalsIgnoreCase(INV_STR)) return TYPE_INVERSION;
+		else if (type.equalsIgnoreCase(INV_DUP_STR)) return TYPE_INV_DUP;
+		else if (type.equalsIgnoreCase(CNV_STR)) return TYPE_CNV;
+		else if (type.equalsIgnoreCase(BND_STR)) return TYPE_BREAKEND;
+		else if (type.equalsIgnoreCase(TRA_STR)) return TYPE_TRANSLOCATION;
+		else return -1;
+	}
+
+}
--- wdl/Sv2Igv.wdl
+++ wdl/Sv2Igv.wdl
@@ -56,46 +56,35 @@ task Sv2IgvImpl {
         
         function downloadThread() {
             local CHUNK_FILE=$1
-            local CHR=$2
-            local START=$3
-            local END=$4
+            local REGION=$2
             
             while read REMOTE_BAM; do
                 SAMPLE_ID=$(basename -s .bam ${REMOTE_BAM})
-                TEST=$(samtools view --threads 1 ${REMOTE_BAM} ${CHR}:${START}-${END} > ${SAMPLE_ID}.sam && echo 0 || echo 1)
+                TEST=$(samtools view --threads 1 ${REMOTE_BAM} ${REGION} > ${SAMPLE_ID}.sam && echo 0 || echo 1)
                 if [ ${TEST} -eq 1 ]; then
                     export GCS_OAUTH_TOKEN=$(gcloud auth application-default print-access-token)
-                    ${TIME_COMMAND} samtools view --threads 1 ${REMOTE_BAM} ${CHR}:${START}-${END} > ${SAMPLE_ID}.sam
+                    ${TIME_COMMAND} samtools view --threads 1 ${REMOTE_BAM} ${REGION} > ${SAMPLE_ID}.sam
                 fi
             done < ${CHUNK_FILE}
         }
         
         tr '\t' ',' < ~{vcf_file} > sv.txt
         CHR=$(cut -d , -f 1 sv.txt)
-        START=$(cut -d , -f 2 sv.txt)
-        START=$(( ${START}-${HORIZONTAL_SLACK} ))
-        if [ ${START} -lt 1 ]; then
-            START="1"
+        if [ ${CHR} = "chr21" ]; then
+            CHR_LENGTH=${CHR21_LENGTH}
+        elif [ ${CHR} = "chr22" ]; then
+            CHR_LENGTH=${CHR22_LENGTH}
         fi
-        END=$(cut -d , -f 3 sv.txt)
-        END=$(( ${END}+${HORIZONTAL_SLACK} ))
-        if [ ${END} -le ${START} ]; then
-            END=$(( ${START}+1 ))
-        fi
-        if [ ${CHR} = "chr21" -a ${END} -gt ${CHR21_LENGTH} ]; then
-            END=$(( ${CHR21_LENGTH} ))
-        elif [ ${CHR} = "chr22" -a ${END} -gt ${CHR22_LENGTH} ]; then
-            END=$(( ${CHR22_LENGTH} ))
-        fi
+        REGION=$(java /Vcf2Region ~{vcf_file} ${HORIZONTAL_SLACK} ${CHR_LENGTH})
         BAM_NAME=$(basename -s .bam ~{bam_addresses})
         N_ROWS=$(wc -l < ~{bam_addresses})
         N_ROWS_PER_CHUNK=$(( ${N_ROWS}/${N_THREADS} ))
         split -l ${N_ROWS_PER_CHUNK} ~{bam_addresses} chunk_
         for CHUNK in chunk_*; do
-            downloadThread ${CHUNK} ${CHR} ${START} ${END} &
+            downloadThread ${CHUNK} ${REGION} &
         done
         wait
-        ${TIME_COMMAND} java -Xmx$((~{ram_size_gb}-4))g /Pigv ~{vcf_file} . ${HORIZONTAL_SLACK} ${CHR22_LENGTH} image.png
+        ${TIME_COMMAND} java -Xmx$((~{ram_size_gb}-4))g /Pigv ~{vcf_file} . ${HORIZONTAL_SLACK} ${CHR_LENGTH} image.png
         while : ; do
             TEST=$(gsutil -m cp './*.png' ~{output_bucket_dir} && echo 0 || echo 1)
             if [ ${TEST} -eq 1 ]; then
